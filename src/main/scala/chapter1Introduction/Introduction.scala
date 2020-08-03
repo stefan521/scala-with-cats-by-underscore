6d42671cab32430a1e4e55f6105dbc48ff415b5c
stefan521 <radu11.sm@gmail.com>
2020-08-03 21:52:03 +0100

    Finished chapter1.

--- src/main/scala/chapter1Introduction/Introduction.scala
+++ src/main/scala/chapter1Introduction/Introduction.scala
@@ -88,7 +88,7 @@ object Introduction extends App {
     implicit class PrintableOps[A](value: A) {
       def format(implicit printable: Printable[A]): String = printable.format(value)
 
-      def print(implicit printable: Printable[A]): Unit = println(printable.format(value))
+      def print(implicit printable: Printable[A]): Unit = println(value.format)
     }
   }
 
@@ -102,11 +102,94 @@ object Introduction extends App {
   case class Cat(name: String, age: Int, color: String)
 
   object Cat {
+    import cats.Show
+
     implicit val printableCat: Printable[Cat] = (cat: Cat) =>
+      s"${cat.name} is a ${cat.age} year-old ${cat.color} cat."
+
+    implicit val catShow: Show[Cat] = (cat: Cat) =>
       s"${cat.name} is a ${cat.age} year-old ${cat.color} cat."
   }
 
   val louie = Cat("Louie", 5, "ginger")
 
   louie.print
+
+  // Meet Cats
+  // step one - TC
+  import cats.Show // cats version of the printable above
+  // step two - instances
+  import cats.instances.int._ // implicit instances of all cats TCs for int. means we can use Show[Int]
+  import cats.instances.string._
+  // step three - syntax
+  import cats.syntax.show._ // for show
+
+  val showInt = Show.apply[Int]
+  val showString: Show[String] = Show.apply[String]
+
+  val intAsString: String = showInt.show(123)
+  1223.show
+
+  val stringAsString: String = showString.show("abc")
+  "abC".show
+
+  import java.util.Date
+
+  implicit val dateShow: Show[Date] = new Show[Date] {
+    def show(date: Date): String = s"${date.getTime}ms since the epoch"
+  }
+
+  println(new Date().show)
+
+  println(louie.show)
+
+  // Cats Eq
+  import cats.Eq
+  import cats.instances.int._
+  import cats.syntax.eq._
+
+  val eqInt = Eq[Int]
+
+  // primitive syntax
+  eqInt.eqv(123, 123)
+  eqInt.eqv(123, 424)
+
+  123 === 123
+  123 =!= 542
+
+  import cats.instances.option._
+  import cats.syntax.option._
+  // Some(1) === None  // breaks cause Some is not same tye as None => dirty fix below
+  (Some(1): Option[Int]) === (None: Option[Int])
+  // not so dirty
+  Option(1) === Option.empty[Int]
+  1.some === none[Int]
+  1.some =!= none[Int]
+
+  import cats.instances.long._ // for Eq
+
+  implicit val dateEq: Eq[Date] = Eq.instance[Date] {
+    (date1, date2) => date1.getTime === date2.getTime
+  }
+
+  val x = new Date()
+  val y = new Date()
+  x === x
+  x === y
+
+  val cat1 = Cat("Garfield", 38, "orange and black")
+  val cat2 = Cat("Heathcliff", 33, "orange and black")
+
+  val optionCat1 = Option(cat1)
+  val optionCat2 = Option.empty[Cat]
+
+  implicit val catEq: Eq[Cat] = Eq.instance[Cat] {
+    (kat1, kat2) =>
+      kat1.name === kat2.name && kat1.age === kat2.age && kat1.color === kat2.color
+  }
+
+  println(cat1 === cat2)
+  println(cat1 =!= cat2)
+
+  optionCat1 === optionCat2
 }
