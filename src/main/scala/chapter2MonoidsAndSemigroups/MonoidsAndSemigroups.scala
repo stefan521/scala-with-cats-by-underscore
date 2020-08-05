e2224c7f3cebdfc45b1cc49e403c6db20781bdd8
stefan521 <radu11.sm@gmail.com>
2020-08-05 21:33:19 +0100

    Finished Monoids and Semigroups.

---
+++ src/main/scala/chapter2MonoidsAndSemigroups/MonoidsAndSemigroups.scala
@@ -0,0 +1,60 @@
+package chapter2MonoidsAndSemigroups
+
+import cats.Monoid
+import cats.instances.int._
+
+object MonoidsAndSemigroups extends App {
+  // Exercise 2.3
+  object Monoid {
+    def apply[A](implicit monoid: Monoid[A]): Monoid[A] = monoid
+  }
+
+  implicit val orMonoid: Monoid[Boolean] = new Monoid[Boolean] {
+    override def empty: Boolean = false
+
+    override def combine(x: Boolean, y: Boolean): Boolean = x || y
+  }
+
+  implicit val andMonoid: Monoid[Boolean] = new Monoid[Boolean] {
+    override def empty: Boolean = true
+
+    override def combine(x: Boolean, y: Boolean): Boolean = x && y
+  }
+
+  implicit val exclusiveOrMonoid: Monoid[Boolean] = new Monoid[Boolean] {
+    override def empty: Boolean = false
+
+    override def combine(x: Boolean, y: Boolean): Boolean = (x && !y) || (!x && y)
+  }
+
+  implicit val booleanExclusiveNorMonoid: Monoid[Boolean] = new Monoid[Boolean] {
+    override def empty: Boolean = true
+
+    override def combine(x: Boolean, y: Boolean): Boolean = (!x || y) && (x || !y)
+  }
+
+  // Exercise 2.4
+  implicit def unionMonoid[A]: Monoid[Set[A]] = new Monoid[Set[A]] {
+    override def empty: Set[A] = Set.empty
+
+    override def combine(x: Set[A], y: Set[A]): Set[A] = x ++ y
+  }
+
+  // Exercise 2.5.4
+  object SuperAdder {
+    case class Order(totalCost: Double, quantity: Double)
+
+    implicit val addOrdersMonoid: Monoid[Order] = new Monoid[Order] {
+      override def empty: Order = Order(0, 0)
+
+      override def combine(x: Order, y: Order): Order = Order(
+        totalCost = x.totalCost + y.totalCost,
+        quantity = x.quantity + y.quantity
+      )
+    }
+
+    def add[A: Monoid](items: List[A]): A = {
+      items.foldLeft(Monoid[A].empty)(Monoid[A].combine)
+    }
+  }
+}
